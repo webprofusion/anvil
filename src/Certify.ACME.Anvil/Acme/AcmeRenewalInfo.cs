f503e02e382a9f0556c40d5e4c70a8231e135f4f
webprofusion-chrisc <christopher.cook@webprofusion.com>
2024-08-02 17:57:44 +0800

    Cleanup

--- src/Certify.ACME.Anvil/Acme/AcmeRenewalInfo.cs
+++ src/Certify.ACME.Anvil/Acme/AcmeRenewalInfo.cs
@@ -4,7 +4,7 @@ using Newtonsoft.Json;
 namespace Certify.ACME.Anvil.Acme
 {
     /// <summary>
-    /// Represents the ACME RenewalInfo resource (draft) https://www.ietf.org/id/draft-ietf-acme-ari-00.html
+    /// Represents the ACME RenewalInfo resource (draft) https://www.ietf.org/id/draft-ietf-acme-ari-04.html
     ///    
     /// {
     ///  "suggestedWindow": {
@@ -33,51 +33,22 @@ namespace Certify.ACME.Anvil.Acme
         public Uri ExplanationURL { get; set; }
 
     }
-}
-
-/// <summary>
-/// ACME RenewalWindow
-/// </summary>
-public class AcmeRenewalWindow
-{
-    /// <summary>
-    /// Start of renewal window
-    /// </summary>
-    [JsonProperty("start")]
-    public DateTimeOffset? Start { get; set; }
 
     /// <summary>
-    /// End of renewal window
+    /// ACME RenewalWindow
     /// </summary>
-    [JsonProperty("end")]
-    public DateTimeOffset? End { get; set; }
-}
-
-/// <summary>
-/// ACME Renewal Update
-/// </summary>
-public class RenewalUpdate
-{
-    /// <summary>
-    /// Id for Certificate
-    /// </summary>
-    [JsonProperty("certId")]
-    public string CertId { get; set; }
-
-    /// <summary>
-    /// If true, certificate has been replaced. Must be set to true for a valid update
-    /// </summary>
-    [JsonProperty("replaced")]
-    public bool Replaced { get; set; }
-
-    /// <summary>
-    /// Represents the payload to update a cert renewal.
-    /// </summary>
-
-    internal class Payload : RenewalUpdate
+    public abstract class AcmeRenewalWindow
     {
+        /// <summary>
+        /// Start of renewal window
+        /// </summary>
+        [JsonProperty("start")]
+        public DateTimeOffset? Start { get; set; }
 
+        /// <summary>
+        /// End of renewal window
+        /// </summary>
+        [JsonProperty("end")]
+        public DateTimeOffset? End { get; set; }
     }
 }
-
-
